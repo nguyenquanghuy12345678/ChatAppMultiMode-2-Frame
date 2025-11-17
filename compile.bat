25b1e57c5b438c673c5bb010ac420e05d3e54ef5
Tran Hoang Nhat <kundeeptryvl@gmail.com>
2025-11-17 23:20:37 +0700

    fix: Update compile script to use Java 20 release flag

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@ if exist bin rmdir /s /q bin
 mkdir bin
 
 echo Compiling project...
-javac -encoding UTF-8 -cp "lib\webcam-capture-0.3.12.jar;lib\slf4j-api-1.7.2.jar;lib\bridj-0.6.2.jar" -d bin src\common\*.java src\server\*.java src\client\*.java
+javac -encoding UTF-8 --release 20 -cp "lib\webcam-capture-0.3.12.jar;lib\slf4j-api-1.7.2.jar;lib\bridj-0.6.2.jar" -d bin src\common\*.java src\server\*.java src\client\*.java
 
 if %errorlevel% equ 0 (
     echo.
